5281ac06a77585d32afbe5eb9ff8ba9f51f07d38
Tito <tito.shafy@student.umn.ac.id>
2025-11-25 09:17:49 +0900

    fixing stuff hopefully, kalo ada yang error, replace aja

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function authenticateToken (req, res, next){
 }
 
 app.get('/', (req, res) => {
-    res.writeHead(2000, {'content-type' : 'text/html'});
+    res.writeHead(200, {'content-type' : 'text/html'});
     res.end();
 })
 
